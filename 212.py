8ffa93c89137f79bba921fc66e298f766b78a223
Subin Cho <109792191+chsubinn@users.noreply.github.com>
2023-01-28 00:42:53 +0900

    Update 212.py

--- 212.py
+++ 212.py
@@ -1,6 +1,7 @@
 # 백준
 # 적록색약
 
+# 1트
 n = int(input())
 graph3 = []
 graph2 = []
@@ -41,5 +42,50 @@ for i in range (n):
             if dfs (i, j, graph2):
                 cnt2+=1
        
-print(cnt3)
+print(cnt3, cnt2)
+
+
+
+# 2트
+n = int(input())
+graph = []
+visited = [[False] * n for _ in range(n)]
+for i in range (n):
+    graph.append(list(map(str, input())))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs (x, y):
+    visited[x][y]=True
+    color = graph[x][y]
+    for k in range (4):
+        nx = x+dx[k]
+        ny = y+dy[k]
+        if (0 <= nx < n) and (0 <= ny < n):
+            if visited[nx][ny]==False:
+                if graph[nx][ny] == color:
+                    dfs(nx,ny)
+        
+cnt3 =0
+cnt2 = 0
+for i in range(n):
+    for j in range(n):
+        if visited[i][j]==False:
+            dfs(i,j)
+            cnt3 += 1
+
+visited = [[False] * n for _ in range(n)]
+for i in range (n):
+    for j in range (n):
+        if graph[i][j]=='G':
+            graph[i][j]='R'
+    
+for i in range(n):
+    for j in range(n):
+        if visited[i][j] == False:
+            dfs(i,j)
+            cnt2 += 1
+
+       
+print(cnt3, end=' ', flush=False )
 print(cnt2)
