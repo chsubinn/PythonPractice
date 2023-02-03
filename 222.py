125db5928703d8d8cb9ea21fff3d502fb96b7e05
Subin Cho <109792191+chsubinn@users.noreply.github.com>
2023-02-04 00:26:37 +0900

    Update 222.py

--- 222.py
+++ 222.py
@@ -17,14 +17,14 @@ def dfs(v, num):
   num += 1
   visited[v] = True
 
-  if v == B:
+  if v == b:
     result.append(num)
 
   for i in graph[v]:
     if not visited[i]:
       dfs(i, num)
 
-dfs(A, 0)
+dfs(a, 0)
 if len(result) == 0:
   print(-1)
 else:
