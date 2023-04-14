532521f35169ea8c60b2c7c113f0626c2e3564ba
Subin Cho <109792191+chsubinn@users.noreply.github.com>
2023-04-15 02:20:11 +0900

    Create 702.py

---
+++ 702.py
@@ -0,0 +1,47 @@
+import heapq
+
+# sys.stdin.readline() 이란 파이썬 내장 함수로 input()을 치환하면,
+# 입력 데이터 수가 많아도 빠르게 동작 가능하다.
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+# 각 노드에 연결된 노드에 대한 정보를 담는 리스트
+graph = [[] for i in range(n+1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q: # 큐가 비어있지 않다면
+        # 최단 거리가 가장 짧은 노드에 대한 정보 꺼내기
+        dist, now = heqpq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print(“INFINITY”)
+    else:
+        print(distance[i])
